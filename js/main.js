75b526c4f8f7ac29fef0491ff5f7a38ebfb967f5
Ives Chiba <97798556+IvesJc@users.noreply.github.com>
2023-09-12 15:18:22 -0300

    Second upload at 12/09
    The JavaScript is able to place a new Pokemon no matter it name, type, img

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 
-const offset = 9
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const pokemons__lista = document.getElementById('pokemon_list')
+const loadMoreButton = document.getElementById("loadMoreButton")
+
+const limit = 10;
+let offset = 0;
+const maxRecords = 15
 
 function convertPokemonHtml(pokemon) {
     return `
-        <li class="pokemons__lista_grass pokemons__lista">
-            <span class="pokemons__lista-number">#001</span>
+        <li class="pokemons__lista_grass pokemons__lista ${pokemon.type}">
+            <span class="pokemons__lista-number">#${pokemon.number}</span>
             <span class="pokemons__lista-name">${pokemon.name}</span>
 
             <div class="pokemons__lista-details">
                 <ol class="pokemons__lista-types">
-                    <li class="pokemons__lista-type_grass">Grass/li>
-                    <li class="pokemons__lista-type_grass">Poison</li>
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.photo}"
                     alt="${pokemon.name}" class="pokemons__lista-img">
             </div>
 
@@ -22,19 +24,29 @@ function convertPokemonHtml(pokemon) {
     `
 }
 
-const pokemons__lista = document.getElementById('pokemon_list')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        for (let i = 0; i < pokemonList.length; i++) {
-            const pokemon = pokemonList[i];
-            pokemons__lista.innerHTML += convertPokemonHtml(pokemon)
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit)
+        .then((pokemonList = []) => {
+            pokemons__lista.innerHTML += pokemonList.map(convertPokemonHtml).join("")
+        })
+}
 
-            
-        }
-    })
+loadPokemonItens(offset, limit)
 
-    .catch((error) => console.log(error))
-    .finally(() => console.log("Requisição Concluída!"))
+
+loadMoreButton.addEventListener("click", () => {
+    offset += limit
+
+    const qtdRegistros = offset + limit
+
+if(qtdRegistros >= maxRecords){
+    const newLimit = maxRecords - offset
+    loadPokemonItens(offset, newLimit)
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+    
+}else{
+    loadPokemonItens(offset, limit)
+}
+})
